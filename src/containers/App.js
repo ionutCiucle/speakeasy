e1995a594b89fb9ead3eac309820fdf1a59f87fa
Unknown <ionut.ciucle@gmail.com>
2018-09-27 21:46:59 +0300

    Add styling directory

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from '../components/Header';
 import { withStyles, MuiThemeProvider } from '@material-ui/core/styles';
-import { speakeasyTheme } from '../theme';
+import Header from '../components/Header';
+import { speakeasyTheme } from '../styling/theme';
 
 function App({ classes }) {
   return (
